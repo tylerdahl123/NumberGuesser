4c9442d85617b2fe91d5977ed61222d665c41924
tyler dahlgren <dahlgren15@gmail.com>
2021-03-22 15:15:25 -0700

    should be good to go

--- script.js
+++ script.js
@@ -2,5 +2,32 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
+function generateTarget(){
+return Math.floor(Math.random() * Math.floor(9));
 
+}
+//console.log(generateTarget());
+function compareGuesses(userGuess, CompGuess, secretTarget){
+const userDiff = Math.abs(secretTarget - userGuess);
+const compDiff = Math.abs(secretTarget - CompGuess);
+return userDiff <= compDiff;
+}
+
+function updateScore(winner){
+if(winner === 'human'){
+    humanScore ++;
+} else if (winner === 'computer'){
+    computerScore ++;
+}
+}
+
+function advanceRound(){
+    currentRoundNumber ++
+}
+
+// updateScore('human');
+// updateScore('human');
+// console.log(humanScore);
+
+// updateScore('computer');
+// console.log(computerScore);
